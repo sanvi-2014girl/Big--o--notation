f86c8fc9ce92988413ad9b98727d01cfbb117de8
sanvi-2014girl <goswamilipi82@gmail.com>
2024-10-26 11:22:59 +0530

    notation

---
+++ main.py
@@ -0,0 +1,8 @@
+def printnumber (n):
+    iteration = 0
+    print("The number entered by the user is" , n)
+    iteration+=1
+    print("Total iterations done by the code is" ,iteration,"\n")
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' the time taken by our code won't change")
